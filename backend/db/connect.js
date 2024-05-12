dabe622f8f401f7db51530f4c3b8026995e5a1db
bhashabridges <paramthakkar864@gmail.com>
2024-05-12 09:04:24 +0530

    initial commit

--- backend/db/connect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-mongoose.connect("mongodb+srv://paramthakkar864:paramthakkar864@cluster0.lj19rgy.mongodb.net/Orders?retryWrites=true&w=majority&appName=Cluster0")
-.then(() => {
-    console.log("DB connection successful")
-}).catch((e) => {
-    console.log("DB connection failed")
-    console.log(e)
-})
