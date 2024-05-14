de50c4b9c698e7261fd4d80ccabffd4017488a23
paramthakkar123 <paramthakkar864@gmail.com>
2024-05-14 16:04:09 +0530

    Files added

--- src/pages/ItemDetailsPage/ItemDetailsPage.jsx
+++ src/pages/ItemDetailsPage/ItemDetailsPage.jsx
@@ -1,20 +1,48 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const ItemDetailsPage = () => {
     const {id} = useParams();
+    const [item, setItem] = useState(null);
+    const [quantity, setQuantity] = useState(1);
 
     useEffect(() => {
         const fetchItemDetails = async () => {
             try{
-                const response = await fetch()
+                const response = await fetch(`http://localhost:5000/getItems/${id}`)
+                const data = await response.json();
+                setItem(data);
             }catch(err){
-                
+                console.error("Error fetching item details", err);
             }
         }
-    }, [])
+
+        fetchItemDetails();
+    }, [id]);
+
+    const handleQuantityChange = (e) => {
+        setQuantity(e.target.value);
+    }
+
+    const handleOrderPlacement = () => {
+
+    }
+
+    if (!item) {
+        return <div>Loading...</div>;
+    }
+
   return (
-    <div>ItemDetailsPage</div>
+  <div>
+    <div>
+        <h2>{item.itemname}</h2>
+        <p>{item.itemdescription}</p>
+        <p>${item.itemprice}</p>
+        <img src={item.itemimageurl} alt={item.itemname} />
+        <input type="number" value={quantity} onChange={handleQuantityChange} />
+        <button onClick={handleOrderPlacement}>Place Order</button>
+    </div>
+  </div>
   )
 }
 
